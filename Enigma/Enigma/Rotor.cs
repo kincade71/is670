404c02ffe73099fe3d60e2218d7b80241ef9de4a
Richard Robinson <kincade71@gmail.com>
2012-06-28 19:58:14 -0700

    Started creating rotor classes

--- Enigma/Enigma/Rotor.cs
+++ Enigma/Enigma/Rotor.cs
@@ -6,9 +6,20 @@ using System.Text;
 
 namespace Enigma
 {
+    //public struct RotorType
+    //{
+    //    public string type;
+    //    public string RotorClass;
+    //    public string[] CharacterMapping;
+    //}
+
     public class Rotor
     {
         public int Id { get; set; }
         public string StringValue { get; set; }
+        public string[] RotorI = { "E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J" };
+        public string[] RotorII = { "A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E" };
+        public string[] RotorIII = { "B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O" };
+        public string[] RotorIV = { };
     }
 }
