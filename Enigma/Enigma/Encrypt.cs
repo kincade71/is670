a3fcdb4798f7a8cfd8906cd4e35a780ee4f0d1fe
Jim Esposito <jamesp4962@students.ecpi.edu>
2012-07-03 21:38:51 -0400

    Added code to encrypt string. Good through reflector and into first rotor coming back

--- Enigma/Enigma/Encrypt.cs
+++ Enigma/Enigma/Encrypt.cs
@@ -20,41 +20,112 @@ namespace Enigma
     {
         // TODO: Implement the collection resource that will contain the Rotor instances
 
-    [WebGet(UriTemplate = "", ResponseFormat = WebMessageFormat.Json)]
-    public List<Rotor> GetCollection()
-    {
+        private void Rotate()
+        {
+            
+        }
+
+        [WebGet(UriTemplate = "", ResponseFormat = WebMessageFormat.Json)]
+
+        public Boolean GetCollection()
+        {
+            Boolean DoubleRotate = false;
             // TODO: Replace the current implementation to return a collection of Rotor instances
-        Rotor x = new Rotor(RotorClass.Rotor, RotorI.charMapping);
-        return new List<Rotor>() { new Rotor() { rotorClass = RotorClass.Rotor, rotorArray = RotorI.charMapping } };
-    }
+            Rotor slot1 = new Rotor(RotorClass.Rotor, RotorType.I, 0);
+            Rotor slot2 = new Rotor(RotorClass.Rotor, RotorType.II, 0);
+            Rotor slot3 = new Rotor(RotorClass.Rotor, RotorType.III, 0);
+            Rotor reflector = new Rotor(RotorClass.Reflector, RotorType.B, 0);
 
-    //    [WebInvoke(UriTemplate = "", Method = "POST")]
-    //    public Rotor Create(Rotor instance)
-    //    {
-    //        // TODO: Add the new instance of Rotor to the collection
-    //        throw new NotImplementedException();
-    //    }
+            string s = "ENIGMA";
+            s = s.ToUpper();
+            char encodedLetter;
+            //plugboard
+            foreach (char c in s)
+            {
+                slot1.currentPosition++;
+                if (slot1.currentPosition > 26)
+                {
+                    slot1.currentPosition = 1;
+                }
+                if (DoubleRotate)
+                {
+                    slot2.currentPosition++;
+                    DoubleRotate = false;
+                }
+                if (slot1.currentPosition == slot1.notchPosition)
+                {
+                    slot2.currentPosition++;
+                    if (slot2.currentPosition > 26)
+                    {
+                        slot2.currentPosition = 1;
+                    }
+                    DoubleRotate = true;
+                    if (slot2.currentPosition == slot2.notchPosition)
+                    {
+                        slot3.currentPosition++;
+                        if (slot3.currentPosition > 26)
+                        {
+                            slot3.currentPosition = 1;
+                        }
+                    }
+                }
+                encodedLetter = ((RotorI)slot1.rotorObject).charMapping[(Convert.ToInt32(c) - 65)+slot1.currentPosition];
+                Int32 nextPosition = (Convert.ToInt32(encodedLetter) - 65 - slot1.currentPosition);
+                if (nextPosition < 0)
+                {
+                    nextPosition = 26;
+                }
+                encodedLetter = ((RotorII)slot2.rotorObject).charMapping[nextPosition];
+                nextPosition = (Convert.ToInt32(encodedLetter) - 65 - slot2.currentPosition);
+                if (nextPosition < 0)
+                {
+                    nextPosition = 26;
+                }
+                encodedLetter = ((RotorIII)slot3.rotorObject).charMapping[nextPosition];
+                nextPosition = (Convert.ToInt32(encodedLetter) - 65 - slot3.currentPosition);
+                if (nextPosition < 0)
+                {
+                    nextPosition = 26;
+                }
+                encodedLetter = ((ReflectorB)reflector.rotorObject).charMapping[nextPosition];
 
-    //    [WebGet(UriTemplate = "{id}")]
-    //    public Rotor Get(string id)
-    //    {
-    //        // TODO: Return the instance of Rotor with the given id
-    //        throw new NotImplementedException();
-    //    }
+                //back through rotors
+                encodedLetter = Convert.ToChar(Convert.ToInt32(encodedLetter) + slot3.currentPosition);
+                char a = Convert.ToChar(Array.IndexOf(((RotorIII)slot3.rotorObject).charMapping,encodedLetter) - slot3.currentPosition + slot2.currentPosition + 65);
+                char b = Convert.ToChar(Array.IndexOf(((RotorIII)slot3.rotorObject).charMapping,a) - slot2.currentPosition + slot1.currentPosition + 65);
+                encodedLetter = Convert.ToChar(Array.IndexOf(((RotorIII)slot3.rotorObject).charMapping, b) - slot1.currentPosition +65);
+            }
 
-    //    [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
-    //    public Rotor Update(string id, Rotor instance)
-    //    {
-    //        // TODO: Update the given instance of Rotor in the collection
-    //        throw new NotImplementedException();
-    //    }
+            return true;//new List<Rotor>() { new Rotor() { rotorClass = slot1.rotorClass, rotorMapping = ((RotorI)(x.rotorObject)).charMapping, startingPosition = x.startingPosition, notchPosition = ((RotorI)(x.rotorObject)).notch, currentPosition = x.currentPosition } };
+        }
 
-    //    [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
-    //    public void Delete(string id)
-    //    {
-    //        // TODO: Remove the instance of Rotor with the given id from the collection
-    //        throw new NotImplementedException();
-    //    }
+        //    [WebInvoke(UriTemplate = "", Method = "POST")]
+        //    public Rotor Create(Rotor instance)
+        //    {
+        //        // TODO: Add the new instance of Rotor to the collection
+        //        throw new NotImplementedException();
+        //    }
+
+        //    [WebGet(UriTemplate = "{id}")]
+        //    public Rotor Get(string id)
+        //    {
+        //        // TODO: Return the instance of Rotor with the given id
+        //        throw new NotImplementedException();
+        //    }
+
+        //    [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        //    public Rotor Update(string id, Rotor instance)
+        //    {
+        //        // TODO: Update the given instance of Rotor in the collection
+        //        throw new NotImplementedException();
+        //    }
+
+        //    [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
+        //    public void Delete(string id)
+        //    {
+        //        // TODO: Remove the instance of Rotor with the given id from the collection
+        //        throw new NotImplementedException();
+        //    }
 
     }
 }
--- Enigma/Enigma/Rotor.cs
+++ Enigma/Enigma/Rotor.cs
@@ -6,52 +6,47 @@ using System.Text;
 
 namespace Enigma
 {
-    //public struct RotorType
-    //{
-    //    public string type;
-    //    public string RotorClass;
-    //    public string[] CharacterMapping;
-    //}
-
-    public struct RotorI
+    public class RotorI
     {
-        public static string[] charMapping = { "E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J" };
-        public static int notch = 18;
+        public char[] charMapping = { 'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J' };
+        public int notch = 18;
     }
-    public struct RotorII
+    public class RotorII
     {
-        public static string[] charMapping = { "A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E" };
-        public static int notch = 6;
+        public char[] charMapping = { 'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E' };
+        public int notch = 6;
     }
-    public struct RotorIII
+    public class RotorIII
     {
-        public static string[] charMapping = { "B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O" };
-        public static int notch = 23;
+        public char[] charMapping = { 'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O' };
+        public int notch = 23;
     }
-    public struct RotorIV
+    public class RotorIV
     {
-        public static string[] charMapping = { "E", "S", "O", "V", "P", "Z", "J", "A", "Y", "Q", "U", "I", "R", "H", "X", "L", "N", "F", "T", "G", "K", "D", "C", "M", "W", "B" };
-        public static int notch = 11;
+        public char[] charMapping = { 'E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B' };
+        public int notch = 11;
     }
-    public struct RotorV
+    public class RotorV
     {
-        public static string[] charMapping = { "V", "Z", "B", "R", "G", "I", "T", "Y", "U", "P", "S", "D", "N", "H", "L", "X", "A", "W", "M", "J", "Q", "O", "F", "E", "C", "K" };
-        public static int notch = 26;
+        public char[] charMapping = { 'V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K' };
+        public int notch = 26;
     }
-    public struct ReflectorA
+    public class ReflectorC
     {
-        public static string[] charMapping = { "E", "J", "M", "Z", "A", "L", "Y", "X", "V", "B", "W", "F", "C", "R", "Q", "U", "O", "N", "T", "S", "P", "I", "K", "H", "G", "D" };
+        public char[] charMapping = { 'E', 'J', 'M', 'Z', 'A', 'L', 'Y', 'X', 'V', 'B', 'W', 'F', 'C', 'R', 'Q', 'U', 'O', 'N', 'T', 'S', 'P', 'I', 'K', 'H', 'G', 'D' };
     }
-    public struct ReflectorB
+    public class ReflectorB
     {
-        public static string[] charMapping = { "Y", "R", "U", "H", "Q", "S", "L", "D", "P", "X", "N", "G", "O", "K", "M", "I", "E", "B", "F", "Z", "C", "W", "V", "J", "A", "T" };
+        public char[] charMapping = { 'Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T' };
     }
     public enum RotorClass { Rotor, Reflector };
+    public enum RotorType { I, II, III, IV, V, B, C };
 
     public class Rotor
     {
         public RotorClass rotorClass { get; set; }
-        public string[] rotorArray { get; set; }
+        public char[] rotorMapping { get; set; }
+        public Object rotorObject; 
         public int startingPosition { get; set; }
         public int currentPosition { get; set; }
         public int notchPosition { get; set; }
@@ -60,10 +55,38 @@ namespace Enigma
         {
         }
 
-        public Rotor(RotorClass rc, string[] ra, int sp)
+        public Rotor(RotorClass rc, RotorType rt, int sp)
         {
             rotorClass = rc;
-            rotorArray = ra;
+            Object ro;
+            switch (rt)
+            {
+                case RotorType.I:
+                    ro = new RotorI();
+                    break;
+                case RotorType.II:
+                    ro = new RotorII(); 
+                    break;
+                case RotorType.III:
+                    ro = new RotorIII(); 
+                    break;
+                case RotorType.IV:
+                    ro = new RotorIV(); 
+                    break;
+                case RotorType.V:
+                    ro = new RotorV(); 
+                    break;
+                case RotorType.B:
+                    ro = new ReflectorB(); 
+                    break;
+                case RotorType.C:
+                    ro = new ReflectorC(); 
+                    break;
+                default:
+                    ro = null;
+                    break;
+            }
+            rotorObject = ro;
             startingPosition = sp;
             currentPosition = startingPosition;
         }
