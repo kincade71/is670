6b4b834c3d9545b7ba4b1c9adde5705661d7b745
Oscar Bernal <oscber6867@gmail.com>
2012-07-12 20:01:44 -0400

    This is the message I have to put in

---
+++ enigmaMachineTests/enigmaMachineTests.h
@@ -0,0 +1,13 @@
+//
+//  enigmaMachineTests.h
+//  enigmaMachineTests
+//
+//  Created by Oscar Bernal on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface enigmaMachineTests : SenTestCase
+
+@end
